fbf9c55e31467c1274db69f275dae4540b720fc7
nicklinyi <linyicas@qq.com>
2020-10-26 09:43:52 +0800

    Create mineos2qssp.py

---
+++ demo/mineos2qssp.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def qka2qp(qka, qmu, cp, cs):
+    coefficient = 4./3.
+    if cs == 0.0:
+        return qka
+    else:
+        inverse_of_Qp = (1. - coefficient*(cs**2)/(cp**2))/qka + coefficient*(cs**2)/(cp**2)/qmu
+        return 1./inverse_of_Qp
+
+
+# stdata = []
+
+fname = 'premQL6i.card'
+fname2 = '1dmodel_axisem_qssp_noQ.txt'
+
+f2 = open(fname2,"w+")
+
+ii = 0
+
+stdata = []
+
+with open(fname,'r') as f:
+    d = f.readlines()
+
+    for i,line in enumerate(d):
+        if i <= 2:
+            header = line.split()
+            print(header)
+            continue
+        else:
+            tmp = line.split()
+            # if tmp[0] == '#':
+            #     continue
+            
+            sdata = list(map(float, tmp))
+            # print(sdata)
+            stdata.append(sdata)
+            # a_float_m = list(a_float_m)
+            # if i % 1000 == 0:
+            # Mineos: radius [m], rho [kg/m^3], vpv [m/s] vsv [m/s] qka , qmu, vph, vsh 
+            # QSSP: NO. depth[km] vp[km/s]  vs[km/s]   ro[g/cm^3] qp         qs
+            # qp = qka2qp(sdata[4], sdata[5], sdata[2], sdata[3])
+            # qs = qmu
+            # qp = 1.0e8
+            # if sdata[5] == 0.0:
+            #     qs = 0.0
+            # else:
+            #     qs = 1.0e8
+            # tmp = '{:>4} {:>9.4f} {:>10.5f} {:>10.5f} {:>10.5f} {:>10.5e} {:>10.5e}'.format(ii, 6371.0 - sdata[0]/1000.0, sdata[2]/1000.0, sdata[3]/1000.0, sdata[1]/1000.0, qp, qs)
+            # f2.write(tmp+'\n')
+    nlayers = len(stdata)
+    for i in range(nlayers):
+        ii += 1
+        qp = 1.0e8
+        if stdata[nlayers-i-1][5] == 0.0:
+            qs = 0.0
+        else:
+            qs = 1.0e8
+        tmp = '{:>4} {:>9.4f} {:>10.5f} {:>10.5f} {:>10.5f} {:>10.5e} {:>10.5e}'.format(ii, 6371.0 - stdata[nlayers-i-1][0]/1000.0, stdata[nlayers-i-1][2]/1000.0, stdata[nlayers-i-1][3]/1000.0, stdata[nlayers-i-1][1]/1000.0, qp, qs)
+        f2.write(tmp+'\n')       
